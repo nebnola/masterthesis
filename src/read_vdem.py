1aaefceeba6f3add457a5abb997ded2cd464ba57
Friedrich <fpagenk@posteo.de>
2025-04-11 23:49:15 +0200

    Notebook to interactively plot all components/variables against each other

--- src/read_vdem.py
+++ src/read_vdem.py
@@ -2,12 +2,14 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-def read_vdem(shuffled=True, only_dm=True, standardize=True):
+def read_vdem(shuffled=True, only_dm=True, only_vdem=True, standardize=True):
     """
     Read V-Dem dataset and return it as a DataFrame
     Args:
         shuffled: If true, the rows are permutated randomly (but consistenly each time)
-        only_dm: If true, only keep dimensions that are used for diffusion map
+        only_dm: If true, only include columns that are used for diffusion map
+        only_vdem: If true, only include columns from the V-Dem dataset proper. Do not include, i.e. diffusion
+        components or principal components
         standardize: If true, standardize data, i.e. scale it to have zero mean and standard deviation one
             For right now, this is only possible if only_dm is True
 
@@ -17,6 +19,10 @@ def read_vdem(shuffled=True, only_dm=True, standardize=True):
     projectroot = Path(__file__).parent.parent
     filename = projectroot / "data" / "data_vdem_dm.csv"
     vdem = pd.read_csv(filename)
+    if only_vdem:
+        use_columns = (["country_name", "country_text_id", "country_id", "year", "COWcode"] +
+                       [col for col in (vdem.columns) if (col.startswith('v2'))])
+        vdem = vdem[use_columns]
     if only_dm:
         use_columns = [col for col in vdem.columns if col.startswith('v2') and col != 'v2x_polyarchy']
         vdem = vdem[use_columns]

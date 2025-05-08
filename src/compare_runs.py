63c9429912a8f027529cfa2ec2319744f1057dfc
Friedrich <fpagenk@posteo.de>
2025-05-08 19:18:04 +0200

    Make different runs more easily comparable
    Always use "ndims" as the variable with the number of components used
    Introduce compare_runs.py to compare different DRDecoders runs

---
+++ src/compare_runs.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Mapping, Iterable
+
+import pandas as pd
+
+from src.dr_decoders import DRDecoders
+
+
+def compare_runs(filenames: Mapping | Iterable, directory="") -> pd.DataFrame:
+    """
+    Compare DRDecoders runs stored in different files and return combined data frame
+    Args:
+        filenames: Either a filename:label mapping or an iterable of filenames. In the second case, the filename
+            itself is used as the label. The label is used in the "method" column of the returned data frame.
+        directory: If given, is prepended to each filename
+
+    Returns:
+        A combined data frame of all DRDecoder runs
+    """
+    dfs = []
+    try:
+        # works if filenames is a dictionary
+        items = filenames.items()
+    except AttributeError:
+        # fallback
+        items = [(fn, fn) for fn in filenames]
+    for fn, label in items:
+        p = Path(directory) / Path(fn)
+        decoders = DRDecoders.from_file(p).decoders
+        decoders["method"] = label
+        dfs.append(decoders)
+    return pd.concat(dfs)
+
+VDEM_RUNS = ["vdem0", "vdem_large_eps", "vdem_nc3_opt", "vdem_pca0", "vdem_tsne"]
+VDEM_DIR = "../data/models"

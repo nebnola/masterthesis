92b8406f3aa25940eaabf953bc7523a32790da7c
Friedrich <fpagenk@posteo.de>
2025-05-28 20:46:11 +0200

    Allow custom label name in compare_runs

--- src/compare_runs.py
+++ src/compare_runs.py
@@ -6,13 +6,14 @@ import pandas as pd
 from src.dr_decoders import DRDecoders
 
 
-def compare_runs(filenames: Mapping | Iterable, directory="") -> pd.DataFrame:
+def compare_runs(filenames: Mapping | Iterable, directory="", label_name="method") -> pd.DataFrame:
     """
     Compare DRDecoders runs stored in different files and return combined data frame
     Args:
         filenames: Either a filename:label mapping or an iterable of filenames. In the second case, the filename
-            itself is used as the label. The label is used in the "method" column of the returned data frame.
+            itself is used as the label. The label is used in the label_name column of the returned data frame.
         directory: If given, is prepended to each filename
+        label_name: The name of the column containing the labels. Default "method"
 
     Returns:
         A combined data frame of all DRDecoder runs
@@ -27,7 +28,7 @@ def compare_runs(filenames: Mapping | Iterable, directory="") -> pd.DataFrame:
     for fn, label in items:
         p = Path(directory) / Path(fn)
         decoders = DRDecoders.from_file(p).decoders
-        decoders["method"] = label
+        decoders[label_name] = label
         dfs.append(decoders)
     return pd.concat(dfs)
 
